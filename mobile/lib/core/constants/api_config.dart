9261eadb5cf5f0edfea6724b8a23f43d4bd603aa
musnymubarak <musnymohammed@gmail.com>
2026-04-17 16:25:05 +0530

    feat: add ApiConfig class with production base URL and timeout constants

--- mobile/lib/core/constants/api_config.dart
+++ mobile/lib/core/constants/api_config.dart
@@ -5,9 +5,9 @@ class ApiConfig {
   /// Android emulator:       http://10.0.2.2:8000/api/v1
   /// Physical device:        http://<YOUR_LAN_IP>:8000/api/v1
   /// VPS Production:       http://37.59.123.98:8005/api/v1
-   static const String baseUrl = 'http://127.0.0.1:8000/api/v1';
+  //static const String baseUrl = 'http://127.0.0.1:8000/api/v1';
 
-  //static const String baseUrl = 'http://37.59.123.98:8005/api/v1';
+  static const String baseUrl = 'http://37.59.123.98:8005/api/v1';
 
   static const Duration connectTimeout = Duration(seconds: 15);
   static const Duration receiveTimeout = Duration(seconds: 15);
